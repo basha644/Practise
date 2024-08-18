9639411c1d5087342f18a2025156879faeafa7ca
basha <akbarbasha.cv@gmail.com>
2024-08-18 22:39:39 +0530

    change

--- Sample.java
+++ Sample.java
@@ -2,6 +2,6 @@ package Practise;
 
  public class Sample{
     public static void main(String[] args) {
-        System.out.println("welcome");
+        System.out.println("Wlcome to java");
     }
 }
