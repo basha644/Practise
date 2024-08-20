42c1a8be2dd64222558b72d6415dee123c1d14fe
basha <akbarbasha.cv@gmail.com>
2024-08-20 20:02:15 +0530

    new change

--- Sample.java
+++ Sample.java
@@ -2,8 +2,10 @@ package Practise;
 
  public class Sample{
     public static void main(String[] args) {
-        System.out.println("Welcome to java");
-        System.out.println("first line");
+        System.out.println("Welcome to java");       
+         System.out.println("Second line");
+         System.out.println("new branch");
+
 
     }
 }
